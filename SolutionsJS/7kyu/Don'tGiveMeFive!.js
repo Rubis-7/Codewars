fe8153c090693e4c7ca56b8b3afdc29e12b9e35a
Rubis-7 <zxc.zxz@yandex.ru>
2020-12-21 19:30:40 +0300

    main
    main

---
+++ SolutionsJS/7kyu/Don'tGiveMeFive!.js
@@ -0,0 +1,7 @@
+function dontGiveMeFive(start, end) {
+    let res = [];
+    for (let i = start; i <= end; i++) {
+        if (!i.toString().includes('5')) res.push(i);
+    }
+    return res.length;
+}
